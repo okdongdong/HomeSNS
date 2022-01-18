d192591b9fee2643dddfbd2a0beb9b723debd350
임창현 <ckdgus777@naver.com>
2022-01-18 09:14:17 +0000

    Revert "Merge branch 'feature/sac' into 'master'"
    This reverts merge request !14

--- backend/HomeSNS/src/main/java/com/ssafy/homesns/service/UserService.java
+++ backend/HomeSNS/src/main/java/com/ssafy/homesns/service/UserService.java
@@ -7,10 +7,5 @@ public interface UserService {
 	public UserResultDto userRegister(UserDto userDto);
 	public UserResultDto userUpdate(UserDto userDto);
 	public UserResultDto userDelete(int userSeq);
-	public UserResultDto userExist(String phone);
-	
-	// 중복체크
-	public UserResultDto checkUserId(String id);
-	public UserResultDto checkUserEmail(String email);
-	public UserResultDto checkUserPhone(String phone);
+	public UserResultDto userExist(UserDto userDto);
 }
