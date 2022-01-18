c6e8720bb54e9814a2333dc2cd3ce399ca953f87
okdongdong <kjch6411@naver.com>
2022-01-19 01:44:26 +0900

    00 fix merge conflict

---
+++ backend/HomeSNS/src/main/java/com/ssafy/homesns/HomeSnsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.homesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSnsApplication.class, args);
+	}
+
+}
