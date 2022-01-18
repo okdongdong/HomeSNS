44dc96ec4cd82310ce173cfc67440a989c31a80d
강동옥 <kjch6411@naver.com>
2022-01-18 16:15:00 +0000

    Revert "Merge branch 'Frontend' into 'master'"
    This reverts merge request !28

---
+++ backend/HomeSNS/src/main/java/com/ssafy/homesns/service/GroupServiceImpl.java
@@ -0,0 +1,70 @@
+package com.ssafy.homesns.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.homesns.dao.GroupDao;
+import com.ssafy.homesns.dto.GroupDto;
+import com.ssafy.homesns.dto.GroupMemberDto;
+import com.ssafy.homesns.dto.GroupResultDto;
+
+@Service
+public class GroupServiceImpl implements GroupService{
+
+	@Autowired
+	GroupDao groupDao;
+	
+	private static final int SUCCESS = 1;
+	private static final int FAIL = -1;
+	
+	@Override
+	public GroupResultDto groupList(int userSeq) {
+		
+		GroupResultDto groupResultDto = new GroupResultDto();
+		
+		try {
+			List<GroupDto> list = groupDao.groupList(userSeq);
+			int count = groupDao.groupListTotalCount();
+			
+			groupResultDto.setGroupDtoList(list);
+			groupResultDto.setCount(count);
+		} catch(Exception e) {
+			e.printStackTrace();
+			groupResultDto.setResult(FAIL);
+		}
+		
+		return groupResultDto;
+	}
+	
+	@Override
+	@Transactional
+	public GroupResultDto groupCreate(GroupDto groupDto) {
+		
+		GroupResultDto groupResultDto = new GroupResultDto();
+		
+		if ( groupDao.groupCreate(groupDto) == 1 ) {
+			groupResultDto.setResult(SUCCESS);
+		} else {
+			groupResultDto.setResult(FAIL);
+		}
+		return groupResultDto;
+	}
+
+	@Override
+	public GroupResultDto groupDelete(GroupMemberDto groupMemberDto) {
+
+		GroupResultDto groupResultDto = new GroupResultDto();
+		
+		if ( groupDao.groupDelete(groupMemberDto) == 1) {
+			groupResultDto.setResult(SUCCESS);
+		} else {
+			groupResultDto.setResult(FAIL);
+		}
+		
+		return groupResultDto;
+	}
+
+}
--- frontend/frontend/src/views/Group/GroupCreate.vue
+++ frontend/frontend/src/views/Group/GroupCreate.vue
@@ -1,28 +1,35 @@
 <template>
-    <v-app class="box" style="background-color:rgba(98,101,232);">
-        <div class="logo-and-title">
-            <img class="logo-img" src="@/assets/logo1.png" alt="logo">
-            <h1 style="color: white;">우리집SNS</h1>
-        </div>
-        <div class="container d-flex">
-            <!-- 그룹 추가하는 곳 -->
-            <div class="row">
-                <div class="col-6">
-                    <img src="@/assets/test1.png" alt="로고 사진입니다." class="group-img">
-                    <h3>그룹 추가</h3>
-                </div>
-                    <!-- data받아오면 for문으로 돌리기 -->
-                <!-- <div class="col-6" v-for="" :key="">
-                    <img :src="${}" alt="그룹 사진입니다." class="group-img" @click="groupMainPage(그룹아이디넣기)">
-                    <h3 style="color:white">{{}}</h3>
-                </div> -->
-                <div class="col-6">
-                    <img src="@/assets/logo2.png" alt="그룹 사진입니다." class="group-img">
-                    <h3>우가우가</h3>
-                </div>
-                <div class="col-6">
-                    <img src="@/assets/logo2.png" alt="그룹 사진입니다." class="group-img">
-                    <h3>우가우가</h3>
+    <v-app class="container box" style="background-color:rgba(98,101,232);">
+        <div class="d-flex flex-column justify-content-center">
+            <img src="@/assets/logo1.png" alt="logo">
+            <div>
+                <h1 style="color: white;">우리집SNS</h1>
+            </div>
+            <div class="container">
+               <!-- 그룹 추가하는 곳 -->
+                <div class="row">
+                    <div class="col-6">
+                        <img src="@/assets/test1.png" alt="로고 사진입니다." class="group-img">
+                        <h3>그룹 추가</h3>
+                    </div>
+                     <!-- data받아오면 for문으로 돌리기 -->
+                    <!-- <div class="col-6" v-for="" :key="">
+                        <img :src="${}" alt="그룹 사진입니다." class="group-img" @click="groupMainPage(그룹아이디넣기)">
+                        <h3 style="color:white">{{}}</h3>
+                    </div> -->
+                   
+                    <div class="col-6">
+                        <img src="@/assets/logo2.png" alt="그룹 사진입니다." class="group-img">
+                        <h3>우가우가</h3>
+                    </div>
+                    <div class="col-6">
+                        <img src="@/assets/logo2.png" alt="그룹 사진입니다." class="group-img">
+                        <h3>우가우가</h3>
+                    </div>
+                    <div class="col-6">
+                        <img src="@/assets/logo2.png" alt="그룹 사진입니다." class="group-img">
+                        <h3>우가우가</h3>
+                    </div>
                 </div>
             </div>
         </div>
@@ -54,24 +61,9 @@ export default {
         text-align: center;
     }
     .group-img {
-        height: 80%;
-        width: 70%;
-        object-fit: cover;
         border: 4px solid white;
         border-radius: 20%;
         box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.329);
-    }
-    .logo-img {
-        height: 40%;
-        width: auto;
-        object-fit: contain;
-    }
-    .logo-and-title{
-        /* height: 30%; */
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        /* align-items: center; */
     }
     h1 {
         color : white;
@@ -79,5 +71,8 @@ export default {
     h3 {
         color : white;
     }
-
+    img {
+        max-width: 60%;
+        height: auto;
+    }
 </style>
