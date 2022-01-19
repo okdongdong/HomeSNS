dc4111bc1a7ee71670454f25a96c30441f579bd7
임창현 <ckdgus777@naver.com>
2022-01-19 17:11:59 +0900

    #S06P12E205-47 feature 로그인 기능 sql 구현 회원가입 기능 sql 구현

--- backend/HomeSNS/src/main/java/com/ssafy/homesns/service/LoginServiceImpl.java
+++ backend/HomeSNS/src/main/java/com/ssafy/homesns/service/LoginServiceImpl.java
@@ -17,6 +17,8 @@ public class LoginServiceImpl implements LoginService{
 	// 비교한 결과, 비밀번호가 일치한다면 유저 정보를 return한다
 	@Override
 	public UserDto login(UserDto dto) {
+		// userDto ==> DB에 저장되어 있는 유저정보
+		// dto ==> 프론트에서 전달받은 로그인 정보
 		UserDto userDto = loginDao.login(dto.getUserId());
 		if ( userDto != null && userDto.getUserPassword().equals(dto.getUserPassword()) ) {
 			return userDto;
