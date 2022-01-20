855525a2cc73123bd39262ebf5927bdaff38e6b9
xogxog <nasa0939@naver.com>
2022-01-20 10:23:50 +0900

    Revert "00 feature"
    This reverts commit 56cdaa4bab3d6b07b418788c435b615e241aa2a1.

--- frontend/frontend/src/store/index.js
+++ frontend/frontend/src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import login from "./modules/login";
-import getMain from "./modules/getMain";
 import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
@@ -13,6 +12,5 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     login,
-    getMain,
   },
 });
