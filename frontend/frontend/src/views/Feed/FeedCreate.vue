3d1bd535b513e6dbe25552747bec757b0deb0516
xogxog <nasa0939@naver.com>
2022-01-21 16:21:26 +0900

    #S06P12E205-28 feature 피드 추가 페이지...해시태그 다 지움

--- frontend/frontend/src/views/Feed/FeedCreate.vue
+++ frontend/frontend/src/views/Feed/FeedCreate.vue
@@ -39,7 +39,7 @@
           <!-- 참석자 명단 인원?-->
           <v-col cols="12">
             <v-combobox
-              v-model="select"
+              v-model="attendPeople"
               :items="items"
               label="참석자 명단"
               multiple
@@ -60,84 +60,6 @@
             </v-textarea>
           </v-col>
           <!-- 해쉬태그 -->
-          <v-col>
-            <v-combobox
-              v-model="model"
-              :filter="filter"
-              :hide-no-data="!search"
-              :items="hashtagitems"
-              :search-input.sync="search"
-              hide-selected
-              label="Search for an option"
-              multiple
-              small-chips
-              solo
-            >
-            <template v-slot:no-data>
-              <v-list-item>
-                <span class="subheading">Create</span>
-                <v-chip
-                  :color="`${colors[nonce - 1]} lighten-3`"
-                  label
-                  small
-                >
-                  {{ search }}
-                </v-chip>
-              </v-list-item>
-            </template>
-            <template v-slot:selection="{ attrs, item, parent, selected }">
-              <v-chip
-                v-if="item === Object(item)"
-                v-bind="attrs"
-                :color="`${item.color} lighten-3`"
-                :input-value="selected"
-                label
-                small
-              >
-                <span class="pr-2">
-                  {{ item.text }}
-                </span>
-                <v-icon
-                  small
-                  @click="parent.selectItem(item)"
-                >
-                  $delete
-                </v-icon>
-              </v-chip>
-            </template>
-            <template v-slot:item="{ index, item }">
-              <v-text-field
-                v-if="editing === item"
-                v-model="editing.text"
-                autofocus
-                flat
-                background-color="transparent"
-                hide-details
-                solo
-                @keyup.enter="edit(index, item)"
-              ></v-text-field>
-              <v-chip
-                v-else
-                :color="`${item.color} lighten-3`"
-                dark
-                label
-                small
-              >
-                {{ item.text }}
-              </v-chip>
-              <v-spacer></v-spacer>
-              <v-list-item-action @click.stop>
-                <v-btn
-                  icon
-                  @click.stop.prevent="edit(index, item)"
-                >
-                  <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
-                </v-btn>
-              </v-list-item-action>
-            </template>
-          </v-combobox>
-          </v-col>
-
 
         </v-row>
       </v-container>
@@ -177,9 +99,9 @@
         rules: {
           name: [val => (val || '').length > 0 || '제목을 입력해주세요'],
         },
-
-        conditions: false,
-        snackbar: false,
+        attendPeople : [], // 참석자 명단
+        // conditions: false,
+        // snackbar: false,
         defaultForm,
         items: [
           '강동옥',
@@ -188,35 +110,6 @@
           '임창현',
           '최이삭',
         ],
-        ////////////// 해쉬태그 영역 ................////////////////
-        activator: null,
-        attach: null,
-        colors: ['green', 'purple', 'indigo', 'cyan', 'teal', 'orange'],
-        editing: null,
-        editingIndex: -1,
-        hashtagitems: [
-          { header: 'Select an option or create one' },
-          {
-            text: 'Foo',
-            color: 'blue',
-          },
-          {
-            text: 'Bar',
-            color: 'red',
-          },
-        ],
-        nonce: 1,
-        menu: false,
-        model: [
-          {
-            text: 'Foo',
-            color: 'blue',
-          },
-        ],
-        x: 0,
-        search: null,
-        y: 0,
-        ////////////////////////////////////////////////
       }
     },
 
@@ -235,55 +128,14 @@
         this.$refs.form.reset()
       },
       submit () {
-        this.snackbar = true
+        // this.snackbar = true
         this.resetForm()
       },
-      // 해쉬태그 영역
-      edit (index, item) {
-        if (!this.editing) {
-          this.editing = item
-          this.editingIndex = index
-        } else {
-          this.editing = null
-          this.editingIndex = -1
-        }
-      },
-      filter (item, queryText, itemText) {
-        if (item.header) return false
-
-        const hasValue = val => val != null ? val : ''
-
-        const text = hasValue(itemText)
-        const query = hasValue(queryText)
-
-        return text.toString()
-          .toLowerCase()
-          .indexOf(query.toString().toLowerCase()) > -1
-      },
-      //해쉬태그 영역
-    },
-
-    // 해쉬태그 영역 //
-    watch: {
-      model (val, prev) {
-        if (val.length === prev.length) return
-
-        this.model = val.map(v => {
-          if (typeof v === 'string') {
-            v = {
-              text: v,
-              color: this.colors[this.nonce - 1],
-            }
-
-            this.items.push(v)
-
-            this.nonce++
-          }
-
-          return v
-        })
-      },
-      // ///////// //
+      deleteHashTag (item) {
+        console.log(this.hashtagitems)
+        console.log(this.model)
+        console.log(item)
+      }
     },
   }
 </script>
