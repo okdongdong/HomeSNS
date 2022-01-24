4977b01b8bb21547d5f95d6f221b162c5db6d84a
xogxog <nasa0939@naver.com>
2022-01-25 02:39:52 +0900

    #00 feature google map 사용위한 import

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -7,10 +7,15 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // google meterial
 import './registerServiceWorker'
+import * as VueGoogleMaps from "vue2-google-maps"
+
 
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
+Vue.use(VueGoogleMaps,
+  { load: { key: process.env.VUE_APP_GOOGLE_MAP_API_KEY, libraries: "places", region: "KR" } }
+);
 
 export default new Vuetify({
   icons: {
