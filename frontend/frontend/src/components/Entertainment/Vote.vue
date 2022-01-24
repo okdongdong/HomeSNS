4e130815a4f0b2b6fd02c7e99485af7b0d74cbeb
okdongdong <kjch6411@naver.com>
2022-01-24 23:55:46 +0900

    #S06P12E205-78 투표기능 UI일부구현
     투표기능 UI일부구현

--- frontend/frontend/src/components/Entertainment/Vote.vue
+++ frontend/frontend/src/components/Entertainment/Vote.vue
@@ -1,94 +1,94 @@
 <template>
-  <v-app>
-    <!-- 투표제목 -->
-
-    <hr />
-    <div>
-      <h3>투표제목:{{ voteInfo.voteTitle }}</h3>
-    </div>
-
+  <div>
     <h3>투표항목</h3>
-    <!-- 투표하기 -->
-    <div v-if="nowVote">
-      <div v-for="(voteItem, idx) in voteInfo.voteItems" :key="idx">
-        <div>
-          {{ voteItem.voteItemName }}
-          {{ voteItem.voteCnt }}
-          {{ idx }}
-        </div>
-
-        <v-btn v-if="voteInfo.myVoteItem===idx" @click.stop="myVote(idx)">투표취소하기</v-btn>
-        <v-btn v-else @click.stop="myVote(idx)">여기 투표하기</v-btn>
-      </div>
-      <hr>
-      <v-btn @click.stop="nowVote = !nowVote">투표완료</v-btn>
-    </div>
 
     <!-- 투표내용보여주기 -->
-    <div v-else>
-      <div v-for="(voteItem, idx) in voteInfo.voteItems" :key="idx">
-        <div>
-          {{ voteItem.voteItemName }}
-          {{ voteItem.voteCnt }}
-          {{ idx }}
+    <!-- 투표하기 -->
+    <div v-for="(voteItem, idx) in info.voteItems" :key="idx">
+      <div>
+        <!-- 투표비율표시 -->
+        <div class="container d-flex">
+          <v-progress-linear
+            class="vote-bar"
+            height="30"
+            :color="info.myVoteItem === idx ? 'rgb(98,101,232)' : '#ABABAB'"
+            :value="votePercent(voteItem.voteCnt)"
+            @click.stop="info.nowVote ? myVote(idx) : pass"
+            ><div class="d-flex justify-space-between container">
+              <div>
+                {{ voteItem.voteItemName }}
+              </div>
+
+              <div class="d-flex align-space-center">
+                <div class="material-icons">person</div>
+                <div class="ml-1">
+                  {{ voteItem.voteCnt }}
+                </div>
+              </div>
+            </div>
+          </v-progress-linear>
+          <v-expand-x-transition>
+            <v-card
+              v-if="info.nowVote"
+              class="align-center d-flex ml-3"
+              flat
+              color="#00000000"
+            >
+              <v-radio
+                :value="idx"
+                @click.stop="myVote(idx)"
+                :checked="info.myVoteItem === idx"
+              ></v-radio>
+            </v-card>
+          </v-expand-x-transition>
         </div>
       </div>
-      <v-btn @click.stop="nowVote = !nowVote">투표하기</v-btn>
     </div>
-  </v-app>
+
+    <div class="justify-center d-flex">
+      <v-btn v-if="info.nowVote" @click.stop="info.nowVote = !info.nowVote"
+        >투표완료</v-btn
+      >
+      <v-btn v-else @click.stop="info.nowVote = !info.nowVote">투표하기</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Vote",
-  // props: {
-  //   voteInfo: Object,
-  //   // VoteInfo: {
-  //   //   voteTitle : '12312312312'
-  //   //   voteItems: [{
-  //   //     voteItemName:'ㅁㅁㅁㄴㄴㅇㅇ"
-  //   //     voteCnt:3
-  //   //     }]
-  //   //   myVoteItem :null
-  //   // }
-  // },
-  data: () => ({
-    nowVote: false,
-    voteInfo: {
-      voteTitle: "12312312312",
-      voteItems: [
-        {
-          voteItemName: "ㅁㅁㅁㄴㄴㅇㅇ",
-          voteCnt: 3,
-        },
-        {
-          voteItemName: "xogus짱짱맨",
-          voteCnt:0,
-        },
-        {
-          voteItemName: "쭈꾸미 비빔밥",
-          voteCnt: 6,
-        },
-      ],
-      myVoteItem: null,
-    },
-  }),
+  props: {
+    info: Object,
+  },
   methods: {
     myVote(idx) {
-      if (this.voteInfo.myVoteItem===null) {
-        this.voteInfo.voteItems[idx].voteCnt += 1;
-        this.voteInfo.myVoteItem = idx;
-      } else if (this.voteInfo.myVoteItem === idx) {
-        this.voteInfo.voteItems[this.voteInfo.myVoteItem].voteCnt -= 1;
-        this.voteInfo.myVoteItem = null;
+      if (this.info.myVoteItem === null) {
+        this.info.voteItems[idx].voteCnt += 1;
+        this.info.myVoteItem = idx;
+      } else if (this.info.myVoteItem === idx) {
+        this.info.voteItems[this.info.myVoteItem].voteCnt -= 1;
+        this.info.myVoteItem = null;
       } else {
-        this.voteInfo.voteItems[this.voteInfo.myVoteItem].voteCnt -= 1;
-        this.voteInfo.voteItems[idx].voteCnt += 1;
-        this.voteInfo.myVoteItem = idx;
+        this.info.voteItems[this.info.myVoteItem].voteCnt -= 1;
+        this.info.voteItems[idx].voteCnt += 1;
+        this.info.myVoteItem = idx;
       }
+    },
+    votePercent(voteCnt) {
+      let voteSum = 0;
+      console.log(voteSum);
+      for (let i = 0; i < this.info.voteItems.length; i++) {
+        voteSum += this.info.voteItems[i].voteCnt;
+      }
+      return (100 * voteCnt) / (voteSum + 0.00001); // zero division 방지
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.vote-bar {
+  border-radius: 20px;
+  transition-duration: 500ms;
+}
+</style>
