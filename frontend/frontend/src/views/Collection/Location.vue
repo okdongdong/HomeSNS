7615f8b94ba04b7b890f0e607e2ba3765fc1d8f0
xogxog <nasa0939@naver.com>
2022-01-24 11:00:56 +0900

    #00 Location.vue bottomnavbar에서 호출시 뼈대 있어야 에러나지 않으므로 뼈대 추가

---
+++ frontend/frontend/src/views/Collection/Location.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'S06p12e205Location',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
