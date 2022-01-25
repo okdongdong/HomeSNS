2561573badb69f10e1b2bbbc4448fce62857ad16
BabSangJune <applause1319@naver.com>
2022-01-25 23:18:07 +0900

    #S06P12E205-86 메인페이지 레이아웃
    #S06P12E205-86 메인페이지 레이아웃 초기

---
+++ frontend/frontend/src/views/Feed/Main.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-card>
+      <v-card-title>2021.12.21</v-card-title>
+      <v-card-subtitle>내용 미쳤어</v-card-subtitle>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 9"
+            :key="n"
+            class="d-flex child-flex"
+            cols="4"
+          >
+            <v-img
+              :src="`https://picsum.photos/200/300`"
+              :lazy-src="`https://picsum.photos/200/300`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row
+                  class="fill-height ma-0"
+                  align="center"
+                  justify="center"
+                >
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "Main",
+      data: () => ({
+        
+    }),
+  };
+</script>
+
+<style scoped>
+
+</style>
