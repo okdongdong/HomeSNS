c081f8ae800401048923c01dc0273d2c5bbfca62
okdongdong <kjch6411@naver.com>
2022-01-25 16:13:40 +0900

    #S06P12E205-90 feature
    미니게임 및 투표정보 조회를 위한 axios요청 추가

--- frontend/frontend/src/views/Entertainment/EntFeedList.vue
+++ frontend/frontend/src/views/Entertainment/EntFeedList.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container">
+    <div class="d-flex justify-space-between align-center">
+      <h1>ENTERTAINMENT</h1>
+      <EntCreatePopup :dialog="dialog" />
+    </div>
     <div v-for="content in contentData" :key="content">
       <EntFeedContainer :content="content" />
     </div>
@@ -8,10 +12,13 @@
 
 <script>
 import EntFeedContainer from "../../components/Entertainment/EntFeedContainer.vue";
+import EntCreatePopup from "../../components/Entertainment/EntCreatePopup.vue";
+import axios from "axios";
 
 export default {
   name: "EntFeedList",
   data: () => ({
+    dialog: false,
     contentData: [
       {
         type: "ghost",
@@ -21,11 +28,54 @@ export default {
           authorPicUrl:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiM_3DwEqCAeTk61eqxYHzcNibjWl3_2fPfQ&usqp=CAU",
           createdAt: "2020.02.02",
+          playerNum: 10,
+          isEdge: [
+            [],
+            [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+            [0, 1, 1, 0, 2, 0, 2, 0, 2, 0, 0],
+            [0, 2, 2, 1, 0, 0, 1, 0, 0, 1, 0],
+            [0, 0, 0, 2, 0, 1, 2, 1, 0, 2, 0],
+            [0, 1, 0, 0, 0, 2, 1, 2, 1, 1, 0],
+            [0, 2, 1, 1, 0, 1, 2, 1, 2, 2, 0],
+            [0, 0, 2, 2, 1, 2, 1, 2, 1, 0, 0],
+            [0, 0, 1, 0, 2, 1, 2, 1, 2, 1, 0],
+            [0, 0, 2, 1, 0, 2, 0, 2, 1, 2, 0],
+            [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          ],
+          playerNames: [
+            0,
+            "강동옥",
+            "박상준",
+            "김태현",
+            "최이삭",
+            "임창현",
+            "강동옥2",
+            "박상준2",
+            "김태현2",
+            "최이삭2",
+            "임창현2",
+          ],
+          resultNames: [
+            0,
+            "결과1",
+            "결과2",
+            "결과3",
+            "결과4",
+            "결과5",
+            "결과6",
+            "결과7",
+            "결과8",
+            "결과9",
+            "결과10",
+          ],
         },
       },
       {
         type: "vote",
         info: {
+          voteId: 1,
           title: "12312312312",
           author: "작성자",
           authorPicUrl:
@@ -54,8 +104,44 @@ export default {
   }),
   components: {
     EntFeedContainer,
+    EntCreatePopup,
   },
-  methods: {},
+  created() {
+    getContent();
+  },
+  beforeUpdate() {
+    getContent();
+  },
+  methods: {
+    getContent() {
+      // 임시용 경로변경
+      this.$router.push({ name: "EntFeedList" });
+
+      const data = {
+        playerNum: 0,
+        isEdge: [],
+        playerNames: [],
+        resultNames: [],
+      };
+      data.playerNum = this.playerNum;
+      for (let i = 1; i <= this.playerNum; i++) {
+        data.isEdge.push(this.isEdge[i]);
+        data.playerNames.push(this.playerNames[i]);
+        data.resultNames.push(this.resultNames[i]);
+      }
+      axios({
+        method: "get",
+        url: `${process.env.VUE_APP_MCS_URL}/ghostleg`,
+        data: data,
+      })
+        .then((res) => {
+          this.contentData = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
 
