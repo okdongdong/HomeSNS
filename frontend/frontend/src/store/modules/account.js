34f1fab9f38260ddc8578a973137d21903915c0b
okdongdong <kjch6411@naver.com>
2022-01-26 14:57:22 +0900

    #00 feature
    그룹선택 -> 메인페이지 링크 임시연결

--- frontend/frontend/src/store/modules/account.js
+++ frontend/frontend/src/store/modules/account.js
@@ -3,20 +3,29 @@ import axios from "axios";
 const account = {
   namespaced: true,
   state: {
+    // 샘플 데이터
     userSeq: 5,
-    userName: '부와아아앜',
-    userImgUrl:'https://pbs.twimg.com/media/CLVCqrsVEAAe9oo.jpg',
+    userName: "부와아아앜",
+    userImgUrl: "https://pbs.twimg.com/media/CLVCqrsVEAAe9oo.jpg",
+    userGroups: [1, 2, 3],
+    nowUserGroup: 1,
     isLogin: localStorage.getItem("jwt") ? true : false,
   },
   mutations: {
     LOGIN: function (state, res) {
       state.isLogin = true;
-      state.userSeq = res.userSeq
-      state.userName = res.userName
-      state.userImgUrl = res.userImgUrl
+      state.userSeq = res.userSeq;
+      state.userName = res.userName;
+      state.userImgUrl = res.userImgUrl;
+      state.userGroups = res.userGroups;
     },
     LOGOUT: function (state) {
       state.isLogin = false;
+      state.userSeq = null;
+      state.userName = null;
+      state.userImgUrl = null;
+      state.userGroups = null;
+      state.nowUserGroup = null;
     },
   },
   actions: {
@@ -35,9 +44,10 @@ const account = {
           console.log(err);
         });
     },
-    logout:function({commit}) {
-      localStorage.removeItem('jwt')
-      commit("LOGOUT")
+    logout: function ({ commit }) {
+      localStorage.removeItem("jwt");
+      commit("LOGOUT");
+      this.$router.push("Login")
     },
     signup: function (credentials) {
       axios({
