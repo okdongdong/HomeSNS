508b0c987626d7525065ae7f21e23a284ea4dad9
okdongdong <kjch6411@naver.com>
2022-01-27 10:20:29 +0900

    #S06P12E205-101 피드 상세페이지
    1. 피드 상세페이지 레이아웃 초안 구현
    2. 피드 router경로 연결
    3. 댓글 및 감정표현 초안 구현

---
+++ frontend/frontend/src/components/Feed/Comment.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='comment-box'>
+    <div class="d-inline-flex">
+      <div>
+        <ProfilePhoto
+          :size="50"
+          :imgUrl="comment.userImgUrl"
+          :name="comment.author"
+        />
+      </div>
+      <div>
+        <div>{{ comment.uploadDate }}</div>
+        <span class="hashtag"> @{{ comment.tag }} </span>
+        <span> {{ comment.content }} </span>
+      </div>
+    </div>
+    <Emotion />
+  </div>
+</template>
+
+<script>
+import ProfilePhoto from "../ProfilePhoto.vue";
+import Emotion from "./Emotion.vue";
+export default {
+  name: "Comment",
+  props: {
+    comment: Object,
+  },
+  components: {
+    ProfilePhoto,
+    Emotion,
+  },
+};
+</script>
+
+<style scoped>
+.comment-box{
+  background-color: rgb(211, 211, 211);
+}
+.hashtag {
+  color: rgb(110, 110, 252);
+}
+</style>
