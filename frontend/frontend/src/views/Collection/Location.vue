3fe9ed7fa9ba281ce944e1186340b8482f9fb7b3
xogxog <nasa0939@naver.com>
2022-02-04 10:52:27 +0900

    S06P12E205-107 feature 그룹 핀 같은 이미지 보이도록 함

--- frontend/frontend/src/views/Collection/Location.vue
+++ frontend/frontend/src/views/Collection/Location.vue
@@ -1,21 +1,29 @@
 <template>
   <v-app>
-    <GmapMap class="map-style" :center="center" :zoom="12"  @click="mark">
-      <gmap-cluster
+    <GmapMap class="gmap-marker-clustering" :center="center" :zoom="12">
+      <GmapCluster
+
         :zoomOnClick="true"
         :enableRetinaIcons="true"
         :minimumClusterSize="3"
+        @click="checkcluster($event)"
+        :styles="clusterStyles"
+
       >
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center = m.position"
-        :clickable="true"
-        :draggable="true"
-        @drag="updateMarker(index, $event.latLng)"
-      />
-      </gmap-cluster>
+              <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          @click="center = m.position"
+          :clickable="true"
+          :icon="{
+            url: m.markerImg,
+            scaledSize: { width: 100, height: 100 },
+            position: m.position,
+          }"
+        />
+      </GmapCluster>
+
     </GmapMap>
   </v-app>
 </template>
@@ -30,78 +38,88 @@ export default {
   },
   data() {
     return {
-      center: { lat: 37.5642135, lng: 127.0016985 }, // 처음 센터 값 (서울)
+       clusterStyles:[
+        {
+          textColor : 'white',
+          url : require('@/assets/pin1.png'),
+          anchorIcon:[70,0],
+          textsize:10
+        },
+        {
+          textColor : 'white',
+          url : require('@/assets/pin1.png'),
+          anchorIcon:[70,0],
+          textsize:10
+        },
+        {
+          textColor : 'white',
+          url : require('@/assets/pin1.png'),
+          anchorIcon:[70,0],
+          textsize:10
+        },
+      ],
+      center: { lat: -31.56391, lng: 147.154312 }, // 처음 센터 값
       currentPlace: null, // 현재위치
       markers: [
         
-          {position : { lat: -31.56391, lng: 147.154312 }},
-          {position : { lat: -33.718234, lng: 150.363181 }},
-          {position : { lat: -33.727111, lng: 150.371124 }},
-          {position : { lat: -33.848588, lng: 151.209834 }},
-          {position : { lat: -33.851702, lng: 151.216968 }},
-          {position : { lat: -34.671264, lng: 150.863657 }},
-          {position : { lat: -35.304724, lng: 148.662905 }},
-          {position : { lat: -36.817685, lng: 175.699196 }},
+          {
+          position: { lat: -31.56391, lng: 147.154312 },
+          markerImg: "https://cdn.iworldtoday.com/news/photo/201603/188668_62908_1934.png",
+        },
+        {
+          position: { lat: -33.718234, lng: 150.363181 },
+          markerImg:
+            "https://images.chosun.com/resizer/KRM2YgbS9kVVz7QMgCMhH5eUaro=/596x795/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/B6DG6T572KT44ELDCUB5IYGTKU.jpg",
+        },
+        {
+          position: { lat: -33.727111, lng: 150.371124 },
+          markerImg: "http://image.newsis.com/2021/07/05/NISI20210705_0000781122_web.jpg",
+        },
+        {
+          position: { lat: -33.848588, lng: 151.209834 },
+          markerImg: "https://news.imaeil.com/photos/2021/03/05/2021030509243023923_l.jpg",
+        },
+        {
+          position: { lat: -33.851702, lng: 151.216968 },
+          markerImg:
+            "https://w.namu.la/s/76fae914da2d988202808b9f68f39d49631dd668dd31403910ed389d13db136fda62fe2be3b7a2d88168359f60d1f4fa780ffcb475c4cccafac35b703cc15a718a59320897465d17bb376dc67a8842601c0b1120428fa86d75ad2f8fce154bae",
+        },
+        {
+          position: { lat: -34.671264, lng: 150.863657 },
+          markerImg: "http://img.segye.com/content/image/2019/04/12/20190412504845.jpg",
+        },
+        {
+          position: { lat: -35.304724, lng: 148.662905 },
+          markerImg: "https://t1.daumcdn.net/cfile/tistory/998E2C4B5E17DD5C32",
+        },
+        {
+          position: { lat: -36.817685, lng: 175.699196 },
+          markerImg: "https://photo.newsen.com/mphoto/2019/07/18/201907181902081910_1.jpg",
+        },
+
         
       ],
     };
   },
-
-  mounted() {
-    // google: gmapApi,
+  created() {
+    this.$gmapApiPromiseLazy().then(() => {
+      this.initialize(); //init once  library has been loaded
+    });
   },
 
   methods: {
-    getCurrLocation() {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const currLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        this.center = currLocation;
-        this.markers.push({ position: currLocation });
-      });
-    },
-    setPlace(place) {
-      this.currentPlace = place;
-    },
-    addMarker() {
-      if (this.currentPlace) {
-        const marker = {
-          lat: this.currentPlace.geometry.location.lat(),
-          lng: this.currentPlace.geometry.location.lng(),
-        };
-        this.markers.push({ position: marker });
-        // this.places = [this.currentPlace]
-        this.center = marker;
-        // this.currentPlace = null;
+       checkcluster(data) {
+      console.log(data.getMarkers());
       }
-    },
-    mark(event) {
-      const marker = {
-        lat: event.latLng.lat(),
-        lng: event.latLng.lng(),
-      };
-      this.markers.push({ position: marker });
-      // this.places = [this.currentPlace]
-      this.center = marker;
-    },
-    updateMarker(index, location) {
-      const marker = {
-        lat: location.lat(),
-        lng: location.lng(),
-      };
-      this.markers.push({ position: marker });
-      // this.places = [this.currentPlace]
-    },
   },
 };
 </script>
 
 <style scoped>
-  .map-style{
-    width: 100%; 
-    height: calc(100% - 160px)
-  }
+.gmap-marker-clustering {
+  width: 100%;
+  height: calc(100vh - 160px);
+}
+
 
 </style>
