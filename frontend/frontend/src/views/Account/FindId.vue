05bac8e4d76a9aeade44c89c06c65f021e532d68
okdongdong <kjch6411@naver.com>
2022-02-11 19:21:50 +0900

    #S06P12E205-128 feature/findId & findpw
    아이디/비밀번호 찾기 로직 구현

--- frontend/frontend/src/views/Account/FindId.vue
+++ frontend/frontend/src/views/Account/FindId.vue
@@ -1,35 +1,58 @@
 <template>
   <v-app class="my-background">
+    <v-overlay :value="nowLoading">
+      <v-progress-circular
+        :size="100"
+        :width="10"
+        color="white"
+        indeterminate
+      ></v-progress-circular>
+    </v-overlay>
+
     <v-spacer></v-spacer>
     <div class="container">
-      <v-card
-        flat
-        color="rgb(0,0,0,0)"
-      >
+      <v-card flat color="rgb(0,0,0,0)">
         <div class="d-flex justify-center">
           <v-img
-            src="@/assets/logo3.png" alt="logo"
+            src="@/assets/logo3.png"
+            alt="logo"
             max-width="200"
             max-height="200"
           >
           </v-img>
         </div>
-        <v-card-title class="main-text justify-center font-weight-bold">아이디 찾기</v-card-title>
+        <v-card-title class="main-text justify-center font-weight-bold"
+          >아이디 찾기</v-card-title
+        >
         <br />
         <div>
-        <v-card-text class="main-text justify-left font-weight-bold">가입 시</v-card-text>
-        <v-card-text class="main-text justify-left font-weight-bold">사용한 이메일로</v-card-text>
-        <v-card-text class="main-text justify-left font-weight-bold">아이디가</v-card-text>
-        <v-card-text class="main-text justify-left font-weight-bold">발송됩니다.</v-card-text>
+          <v-card-text class="main-text justify-left font-weight-bold"
+            >가입 시</v-card-text
+          >
+          <v-card-text class="main-text justify-left font-weight-bold"
+            >사용한 이메일로</v-card-text
+          >
+          <v-card-text class="main-text justify-left font-weight-bold"
+            >아이디가</v-card-text
+          >
+          <v-card-text class="main-text justify-left font-weight-bold"
+            >발송됩니다.</v-card-text
+          >
         </div>
       </v-card>
     </div>
     <br />
     <br />
-    
+
     <div class="container">
       <v-row justify="center">
-        <v-form class="form-data" ref="form" v-model="valid" lazy-validation>
+        <v-form
+          class="form-data"
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          @keyup.enter="findId()"
+        >
           <!-- 이메일 -->
           <v-text-field
             clearable
@@ -39,6 +62,7 @@
             :rules="rules.emailRules"
             label="이메일"
             required
+            @keyup.enter="findId()"
           ></v-text-field>
           <br />
           <v-btn
@@ -48,7 +72,7 @@
             :disabled="!valid"
             color="black"
             class="mr-4"
-            @click="login()"
+            @click="findId()"
             width="100%"
           >
             아이디 찾기
@@ -72,9 +96,12 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "FindId",
   data: () => ({
+    nowLoading: false,
     valid: true,
     credentials: {
       email: null,
@@ -91,27 +118,45 @@ export default {
     move(name) {
       this.$router.push({ name: name });
     },
+    findId() {
+      this.nowLoading = true;
+      axios({
+        method: "GET",
+        url: `${process.env.VUE_APP_MCS_URL}/user/find/id/${this.credentials.email}`,
+      })
+        .then((res) => {
+          console.log(res);
+          console.log(res.data);
+          this.$router.push("Login");
+          this.nowLoading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log(err.response);
+          this.nowLoading = false;
+        });
+    },
   },
 };
 </script>
 
 <style scoped>
-  .my-background {
-    background-color: rgba(0,0,0,0);
-  }
+.my-background {
+  background-color: rgba(0, 0, 0, 0);
+}
 
-  .form-data {
-    width: 80%;
-  }
+.form-data {
+  width: 80%;
+}
 
-  .main-text {
-    color: #fff !important;
-    font-size: 30px;
-  }
+.main-text {
+  color: #fff !important;
+  font-size: 30px;
+}
 
-  .text-back {
-    text-decoration: none;
-    color: #fff;
-    display: block;
-  }
+.text-back {
+  text-decoration: none;
+  color: #fff;
+  display: block;
+}
 </style>
--- frontend/frontend/src/views/Account/FindPassword.vue
+++ frontend/frontend/src/views/Account/FindPassword.vue
@@ -1,42 +1,59 @@
 <template>
-  <v-app class="my-background"
-  ><v-spacer></v-spacer>
+  <v-app class="my-background">
+    <v-overlay :value="nowLoading">
+      <v-progress-circular
+        :size="100"
+        :width="10"
+        color="white"
+        indeterminate
+      ></v-progress-circular>
+    </v-overlay>
+
+    <v-spacer></v-spacer>
     <div class="container">
-      <v-card
-        flat
-        color="rgb(0,0,0,0)"
-      >
+      <v-card flat color="rgb(0,0,0,0)">
         <div class="d-flex justify-center">
           <v-img
-            src="@/assets/logo3.png" alt="logo"
+            src="@/assets/logo3.png"
+            alt="logo"
             max-width="200"
             max-height="200"
           >
           </v-img>
         </div>
-        <v-card-title class="main-text justify-center font-weight-bold">비밀번호 찾기</v-card-title>
+        <v-card-title class="main-text justify-center font-weight-bold"
+          >비밀번호 찾기</v-card-title
+        >
         <br />
-        <v-card-text class="main-text justify-left font-weight-bold">가입 시</v-card-text>
-        <v-card-text class="main-text justify-left font-weight-bold">사용한 이메일로</v-card-text>
-        <v-card-text class="main-text justify-left font-weight-bold">비밀번호가</v-card-text>
-        <v-card-text class="main-text justify-left font-weight-bold">발송됩니다.</v-card-text>
+        <v-card-text class="main-text justify-left font-weight-bold"
+          >가입 시</v-card-text
+        >
+        <v-card-text class="main-text justify-left font-weight-bold"
+          >사용한 이메일로</v-card-text
+        >
+        <v-card-text class="main-text justify-left font-weight-bold"
+          >비밀번호가</v-card-text
+        >
+        <v-card-text class="main-text justify-left font-weight-bold"
+          >발송됩니다.</v-card-text
+        >
       </v-card>
     </div>
     <br />
     <br />
-    
+
     <div class="container">
       <v-row justify="center">
         <v-form class="form-data" ref="form" v-model="valid" lazy-validation>
           <!-- 아이디 -->
           <v-text-field
-          clearable
-          background-color="white"
-          solo
-          v-model="credentials.id"
-          :rules="rules.idRules"
-          label="아이디"
-          required
+            clearable
+            background-color="white"
+            solo
+            v-model="credentials.id"
+            :rules="rules.idRules"
+            label="아이디"
+            required
           ></v-text-field>
           <!-- 이메일 -->
           <v-text-field
@@ -56,12 +73,12 @@
             :disabled="!valid"
             color="black"
             class="mr-4"
-            @click="login()"
+            @click="findPassword()"
             width="100%"
           >
             비밀번호 찾기
           </v-btn>
-        <v-btn
+          <v-btn
             text
             dark
             large
@@ -80,9 +97,11 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "FindPassword",
   data: () => ({
+    nowLoading: false,
     valid: true,
     credentials: {
       email: null,
@@ -108,27 +127,49 @@ export default {
     move(name) {
       this.$router.push({ name: name });
     },
+    findPassword() {
+      this.nowLoading = true;
+      axios({
+        method: "GET",
+        url: `${process.env.VUE_APP_MCS_URL}/user/find/pw/`,
+        params: {
+          userEmail: this.credentials.email,
+          userId: this.credentials.id,
+        },
+      })
+        .then((res) => {
+          this.nowLoading = false;
+          console.log(res);
+          console.log(res.data);
+          this.$router.push("Login");
+        })
+        .catch((err) => {
+          this.nowLoading = false;
+          console.log(err);
+          console.log(err.response);
+        });
+    },
   },
 };
 </script>
 
 <style scoped>
-  .my-background {
-    background-color: rgba(0,0,0,0);
-  }
+.my-background {
+  background-color: rgba(0, 0, 0, 0);
+}
 
-  .form-data {
-    width: 80%;
-  }
+.form-data {
+  width: 80%;
+}
 
-  .main-text {
-    color: #fff !important;
-    font-size: 30px;
-  }
+.main-text {
+  color: #fff !important;
+  font-size: 30px;
+}
 
-  .text-back {
-    text-decoration: none;
-    color: #fff;
-    display: block;
-  }
+.text-back {
+  text-decoration: none;
+  color: #fff;
+  display: block;
+}
 </style>
