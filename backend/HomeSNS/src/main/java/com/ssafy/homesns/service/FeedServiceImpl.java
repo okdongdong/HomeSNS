047c3c6b823b7842de0afad6a16429558d88db08
최이삭 <isaac950619@gmail.com>
2022-02-16 00:27:27 +0900

    #00 feature/backend
    피드작성시 feedEmotionCreate호출

--- backend/HomeSNS/src/main/java/com/ssafy/homesns/service/FeedServiceImpl.java
+++ backend/HomeSNS/src/main/java/com/ssafy/homesns/service/FeedServiceImpl.java
@@ -247,7 +247,7 @@ public class FeedServiceImpl implements FeedService {
 					feedDao.feedFileInsert(fileDto);
 				}
 			}
-
+			feedDao.feedEmotionCreate(feedId);
 			System.out.println("Service feedDto --------- ");
 			System.out.println(feedDto);
 			feedResultDto.setResult(SUCCESS);
